5b6df47401c0004f5d130deb30e385a5df76cf2c
Daniel Collado <dcolladovizcarro@gmail.com>
2023-05-05 00:52:09 +0200

    Add ArrayToString.java

---
+++ Kata 8/ArrayToString/ArrayToString.java
@@ -0,0 +1,34 @@
+/* Kata Level: 8.
+ * 
+ * Sentence Smash
+ * Write a function that takes an array of words and smashes them together 
+ * into a sentence and returns the sentence. You can ignore any need to sanitize 
+ * words or add punctuation, but you should add spaces between each word. 
+ * Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+ * 
+ * Example:
+ * ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+ */
+
+ public class ArrayToString {
+
+	public static String smash(String... words) {
+		
+		String texto = "";
+		for(String word : words) {
+			texto += word + " ";
+		}
+		texto = texto.trim();
+		while(texto.endsWith(" ")) {
+			texto = texto.substring(0, texto.length()-1);
+		}
+		return texto;
+	}
+	
+	public static void main(String[] args) {
+		
+		String[] palabras = {" ", "hola", "que", "tal", "estas", " "};
+		System.out.println(smash(palabras));
+	}
+
+}
