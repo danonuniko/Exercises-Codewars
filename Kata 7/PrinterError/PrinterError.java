f837ee7abd126f3f6723c69df3a290507ca83d65
Daniel Collado <dcolladovizcarro@gmail.com>
2023-05-08 23:09:11 +0200

    Add PrinterError.java

---
+++ Kata 7/PrinterError/PrinterError.java
@@ -0,0 +1,52 @@
+/* Kata Level: 7.
+ * 
+ * In a factory a printer prints labels for boxes. 
+ * For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+ * The colors used by the printer are recorded in a control string. 
+ * For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, 
+ * four times color b, one time color h then one time color a...
+ * Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced 
+ * e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+ * You have to write a function printer_error which given a string will return the error rate of the printer as a string 
+ * representing a rational whose numerator is the number of errors and the denominator the length of the control string. 
+ * Don't reduce this fraction to a simpler expression.
+ * The string has a length greater or equal to one and contains only letters from ato z.
+ * 
+ * Examples:
+ * 
+ * s="aaabbbbhaijjjm"
+ * printer_error(s) => "0/14"
+ * 
+ * s="aaaxbbbbyyhwawiwjjjwwm"
+ * printer_error(s) => "8/22"
+ */
+
+ public class PrinterError {
+	
+	// Mi solución.
+	public static String printerError(String s) {
+		int errores = 0;
+		char[] letras = s.toCharArray();
+		for (char letra : letras) {
+			if (letra != 'a' && letra != 'b' && letra != 'c' && letra != 'd' && letra != 'e' && letra != 'f'
+					&& letra != 'g' && letra != 'h' && letra != 'i' && letra != 'j' && letra != 'k' && letra != 'l'
+					&& letra != 'm') {
+				errores += 1;
+			}
+		}
+		return errores + "/" + s.length();
+	}
+
+	// Solución de la comunidad.
+	public static String printerError1(String s) {
+		return s.replaceAll("[a-m]", "").length() + "/" + s.length();
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println(printerError("aaabbbbhaijjjm"));
+		System.out.println(printerError("aaaxbbbbyyhwawiwjjjwwm"));
+		
+	}
+
+}
