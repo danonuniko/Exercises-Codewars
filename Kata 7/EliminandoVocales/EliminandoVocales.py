924bf231145d74eb04cfd3ddf30bf33595bb7ad8
Daniel Collado <dcolladovizcarro@gmail.com>
2023-05-10 01:15:47 +0200

    Add EliminandoVocales.py

---
+++ Kata 7/EliminandoVocales/EliminandoVocales.py
@@ -0,0 +1,31 @@
+"""
+Kata Level: 7.
+
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+"""
+
+# Solución propia de Python.
+
+def disemvowel(string):
+    vocales = "aeiouáéíóúüAEIOUÁÉÍÓÚÜ"
+    result = ""
+    for letter in string:
+        if letter not in vocales:
+            result += letter
+    return result 
+
+# Solución de la comunidad 1.
+
+def disemvowel(s):
+    for i in "aeiouAEIOU":
+        s = s.replace(i,'')
+    return s
+
+print(disemvowel("This website is for losers LOL!"))
