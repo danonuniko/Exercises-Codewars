422cb495c390734f1142315ee61cef39d7ef8167
Daniel Collado <dcolladovizcarro@gmail.com>
2023-05-11 11:14:35 +0200

    Add ReversedString.java

---
+++ Kata 8/ReversedString/ReversedString.py
@@ -0,0 +1,20 @@
+"""
+Kata Level: 8.
+
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+"""
+
+# Mi solución en Python.
+
+def solution(string):
+    return string[::-1]
+    
+    #How it works:
+        #for char in range(len(string)-1,-1,-1):
+            #return string[char]        Diría que más que return deberíamos añadirlo a un string auxiliar, y una vez acabe el for devolver dicho string auxiliar.
+
+print(solution("world"))
+print(solution("word"))
