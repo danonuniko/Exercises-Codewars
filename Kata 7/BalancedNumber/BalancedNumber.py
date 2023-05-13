24096dd6eaa87b44f148303ed940464c0dbb0328
Daniel Collado <dcolladovizcarro@gmail.com>
2023-05-13 22:04:35 +0200

    Add BalancedNumber.py

---
+++ Kata 7/BalancedNumber/BalancedNumber.py
@@ -0,0 +1,67 @@
+"""
+Kata Level: 7.
+
+A balanced number is a number where the sum of digits to the left of the middle digit(s) 
+and the sum of digits to the right of the middle digit(s) are equal.
+If the number has an odd number of digits, then there is only one middle digit. (For example, 92645 has one middle digit, 6.) 
+Otherwise, there are two middle digits. (For example, the middle digits of 1301 are 3 and 0.)
+
+The middle digit(s) should not be considered when determining whether a number is balanced or not, 
+e.g. 413023 is a balanced number because the left sum and right sum are both 5.
+
+The task:
+Given a number, find if it is balanced, and return the string "Balanced" or "Not Balanced" accordingly. 
+The passed number will always be positive.
+
+Examples:
+
+7 ==> return "Balanced"
+Explanation:
+middle digit(s): 7
+sum of all digits to the left of the middle digit(s) -> 0
+sum of all digits to the right of the middle digit(s) -> 0
+0 and 0 are equal, so it's balanced.
+
+295591 ==> return "Not Balanced"
+Explanation:
+middle digit(s): 55
+sum of all digits to the left of the middle digit(s) -> 11
+sum of all digits to the right of the middle digit(s) -> 10
+11 and 10 are not equal, so it's not balanced.
+
+959 ==> return "Balanced"
+Explanation:
+middle digit(s): 5
+sum of all digits to the left of the middle digit(s) -> 9
+sum of all digits to the right of the middle digit(s) -> 9
+9 and 9 are equal, so it's balanced.
+
+27102983 ==> return "Not Balanced"
+Explanation:
+middle digit(s): 02
+sum of all digits to the left of the middle digit(s) -> 10
+sum of all digits to the right of the middle digit(s) -> 20
+10 and 20 are not equal, so it's not balanced.
+"""
+
+# Mi solución en Python.
+
+def balanced_num(number):
+    lista = []
+    izquierda = 0
+    derecha = 0
+    for digit in str(number):
+        lista.append(int(digit))
+    if len(lista)% 2:
+        izquierda = sum(x for x in lista[:len(lista) // 2])
+        derecha = sum(x for x in lista[len(lista) // 2 + 1:])
+        return "Balanced" if izquierda == derecha  else "Not Balanced"
+    else:
+        izquierda = sum(x for x in lista[:len(lista) // 2 - 1])
+        derecha = sum(x for x in lista[len(lista) // 2 + 1:])
+        return "Balanced" if izquierda == derecha  else "Not Balanced"
+    
+print(balanced_num(7))
+print(balanced_num(295591))
+print(balanced_num(959))
+print(balanced_num(27102983))
