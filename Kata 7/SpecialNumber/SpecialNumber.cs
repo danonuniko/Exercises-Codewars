4aaeb6422ca778b34a99e5ec977afb58c34f2018
Daniel Collado <dcolladovizcarro@gmail.com>
2023-05-14 04:02:43 +0200

    Add SpecialNumber.cs

---
+++ Kata 7/SpecialNumber/SpecialNumber.cs
@@ -0,0 +1,49 @@
+/* Kata Level: 7.
+ *
+ * Definition:
+ * A number is a Special Number if it’s digits only consist 0, 1, 2, 3, 4 or 5.
+ * 
+ * Given a number determine if it special number or not.
+ * 
+ * Notes:
+ * The number passed will be positive (N > 0) .
+ * All single-digit numbers within the interval [1:5] are considered as special number.
+ * 
+ * Input >> Output Examples:
+ * 
+ * specialNumber(2) ==> return "Special!!"
+ * Explanation: It's a single-digit number within the interval [1:5].
+ * 
+ * specialNumber(9) ==> return "NOT!!"
+ * Explanation: Although, it's a single-digit number but Outside the interval [1:5] .
+ * 
+ * specialNumber(23) ==> return "Special!!"
+ * Explanation: All the number's digits formed from the interval [0:5] digits .
+ * 
+ * specialNumber(39) ==> return "NOT!!"
+ * Explanation: Although, there is a digit (3) Within the interval But the second digit is not (Must be ALL The Number's Digits).
+ * 
+ * specialNumber(59) ==> return "NOT!!"
+ * Explanation: Although, there is a digit (5) Within the interval But the second digit is not (Must be ALL The Number's Digits) .
+ * 
+ * specialNumber(513) ==> return "Special!!"
+ * specialNumber(709) ==> return "NOT!!"
+ */
+
+using System.Text.RegularExpressions;
+
+public class Kata
+{
+    public static string SpecialNumber(int number)
+    {
+        var rx = new Regex("^[0-5]+$");
+		bool solution = rx.IsMatch(number.ToString());
+		return solution ? "Special!!" : "NOT!!";
+
+        /*  
+        Otra opción simplificada de Regex pero que me daba error (Creo que por no utilizar el método ToString()): 
+            return Regex.IsMatch(number.ToString(), "[^0-5]") ? "NOT!!" : "Special!!";
+        Lo que no entiendo es porque en este caso el orden de true y false del operador ternario parece funcionar al revés.
+        */
+    }
+}
