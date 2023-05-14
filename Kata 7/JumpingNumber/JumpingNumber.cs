d57ddc608552d1d6f3fc18c63b649bb99f3a8512
Daniel Collado <dcolladovizcarro@gmail.com>
2023-05-14 03:21:53 +0200

    Add community solutions JumpingNumber.cs

--- Kata 7/JumpingNumber/JumpingNumber.cs
+++ Kata 7/JumpingNumber/JumpingNumber.cs
@@ -56,6 +56,23 @@ public class Kata
       }
     }
 
+    // Soluciones de la comunidad.
+
+    public static string JumpingNumberCommunity1(int n)
+    {
+        while (true)
+        {
+            if (n < 10) return "Jumping!!";
+            if (Math.Abs(n % 10 - n / 10 % 10) != 1) return "Not!!";
+            n /= 10;
+        }
+    }
+	
+	  public static string JumpingNumberCommunity2(int n)
+    {
+        return n < 10 ? "Jumping!!" : Math.Abs(n%10 - (n/10)%10) != 1 ? "Not!!" : JumpingNumber(n/10);
+    }
+
     public static void Main(string[] args)
     {
         Console.WriteLine(JumpingNumber(3565678));          // Not!!
