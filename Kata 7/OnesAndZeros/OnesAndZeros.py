f43e9ea95b83f80c1b48b2cb04e2d5423350dfb0
Daniel Collado <dcolladovizcarro@gmail.com>
2023-05-17 00:28:49 +0200

    Add OnesAndZeros.py

---
+++ Kata 7/OnesAndZeros/OnesAndZeros.py
@@ -0,0 +1,34 @@
+"""
+Kata Level: 7.
+
+Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+Examples:
+
+Testing: [0, 0, 0, 1] ==> 1
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 0, 1] ==> 5
+Testing: [1, 0, 0, 1] ==> 9
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 1, 0] ==> 6
+Testing: [1, 1, 1, 1] ==> 15
+Testing: [1, 0, 1, 1] ==> 11
+
+However, the arrays can have varying lengths, not just limited to 4.
+"""
+
+# Mi solución en Python.
+
+def binary_array_to_number(arr):
+    arr = reversed(arr)
+    result = 0
+    for i, number in enumerate(arr):
+        if number == 1:
+            result += 2**i
+    return result
+
+print(binary_array_to_number([0, 0, 0, 1]))
+print(binary_array_to_number([0, 0, 1, 0]))
+print(binary_array_to_number([0, 1, 0, 1]))
+print(binary_array_to_number([1, 0, 0, 1]))
