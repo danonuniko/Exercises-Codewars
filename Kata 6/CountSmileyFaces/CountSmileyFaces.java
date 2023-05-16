bee98467a8a2f2e308a83a1991d5143ece9c380c
Daniel Collado <dcolladovizcarro@gmail.com>
2023-05-16 08:54:55 +0200

    add CountSmileyFaces.java

---
+++ Kata 6/CountSmileyFaces/CountSmileyFaces.java
@@ -0,0 +1,65 @@
+/* Kata Level: 6.
+ * 
+ * Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+ * 
+ * Rules for a smiling face:
+ * Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+ * A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+ * Every smiling face must have a smiling mouth that should be marked with either ) or D
+ * No additional characters are allowed except for those mentioned.
+ * 
+ * Valid smiley face examples: :) :D ;-D :~)
+ * Invalid smiley faces: ;( :> :} :]
+ * 
+ * Example:
+ * countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+ * countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+ * countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+ * 
+ * Note:
+ * In case of an empty array return 0. You will not be tested with invalid input (input will always be an array).
+ * Order of the face (eyes, nose, mouth) elements will always be the same.
+ */
+
+import java.util.*;
+
+public class CountSmileyFaces {
+
+	/*	Mi solución. Pasa los test pero es errónea, pues dará como válidos caras del
+	 *  estilo ;- , :-, D, ;~, ~D, etc.
+	 */
+	public static int countSmileys(List<String> arr) {
+		String opciones = ":) ;) :-) :~) ;-) ;~) :D ;D :-D :~D ;-D ;~D";
+		int contador = 0;
+		for (String cara : arr) {
+			if (opciones.contains(cara))
+				contador++;
+		}
+		return contador;
+	}
+
+	// Solución de la comunidad.
+	public static int countSmileys1(List<String> arr) {
+		String validSmileRegExp = "[:;][-~]?[)D]";
+		int smiles = 0;
+		for (String smile : arr) {
+			if (smile.matches(validSmileRegExp)) {
+				smiles += 1;
+			}
+		}
+		return smiles;
+	}
+
+	public static void main(String[] args) {
+
+		List<String> arr = new ArrayList<>();
+		arr.add(":)");
+		arr.add(";(");
+		arr.add(";}");
+		arr.add(":-D");
+		arr.add("-D"); // Con mi solución este String lo cuenta como cara cuando no debería. ESTUDIAR solución de la comunidad.
+		System.out.println(countSmileys(arr));
+
+	}
+
+}
