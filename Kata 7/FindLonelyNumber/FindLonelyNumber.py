b3cb163bcb520aec0bda344664d3a16f8ae94cfc
Daniel Collado <dcolladovizcarro@gmail.com>
2023-05-18 00:22:02 +0200

    Add FindLonelyNumber.py

---
+++ Kata 7/FindLonelyNumber/FindLonelyNumber.py
@@ -0,0 +1,24 @@
+"""
+Kata Level: 7.
+
+You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+Complete the method which accepts such an array, and returns that single different number.
+
+The input array will always be valid! (odd-length >= 3)
+
+Examples:
+[1, 1, 2] ==> 2
+[17, 17, 3, 17, 17, 17, 17] ==> 3
+"""
+
+def stray(arr):
+    arr.sort()
+    return arr[-1] if arr[0] == arr[1] else arr[0]
+
+def stray_community(arr):
+    for x in arr:
+        if arr.count(x) == 1:
+            return x
+
+print(stray([1, 1, 2]))
+print(stray([17, 17, 3, 17, 17, 17, 17]))
