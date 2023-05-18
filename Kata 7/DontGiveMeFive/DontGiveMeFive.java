88671f3531810098b074516185b9091556483d90
Daniel Collado <dcolladovizcarro@gmail.com>
2023-05-18 03:18:32 +0200

    Add examples DontGiveMeFive.java

--- Kata 7/DontGiveMeFive/DontGiveMeFive.java
+++ Kata 7/DontGiveMeFive/DontGiveMeFive.java
@@ -27,6 +27,7 @@
 	public static void main(String[] args) {
 		System.out.println(dontGiveMeFive(1, 9));
 		System.out.println(dontGiveMeFive(4, 17));
+		System.out.println(dontGiveMeFive(50, 60));
 	}
  
 }
