c738fab383a4b86e879a8fe2a82c4b7e361e4bb2
Daniel Collado <dcolladovizcarro@gmail.com>
2023-05-21 03:44:58 +0200

    Format code GoodVsEvil.java

--- Kata 6/GoodVsEvil/GoodVsEvil.java
+++ Kata 6/GoodVsEvil/GoodVsEvil.java
@@ -78,8 +78,7 @@ public class GoodVsEvil {
         int[] evil = Arrays.stream(evilAmounts.split(" ")).mapToInt(i -> Integer.parseInt(i)).toArray();
         int goodSum = IntStream.rangeClosed(0,good.length-1).map(i->good[i]*goodWorth[i]).sum();
         int evilSum = IntStream.rangeClosed(0,evil.length-1).map(i->evil[i]*evilWorth[i]).sum();
-        return goodSum > evilSum ?  "Battle Result: Good triumphs over Evil" :
-                evilSum > goodSum ? "Battle Result: Evil eradicates all trace of Good" :
-                                    "Battle Result: No victor on this battle field";
+        return  goodSum > evilSum ?  "Battle Result: Good triumphs over Evil" :
+                evilSum > goodSum ? "Battle Result: Evil eradicates all trace of Good" : "Battle Result: No victor on this battle field";
     }
 }
