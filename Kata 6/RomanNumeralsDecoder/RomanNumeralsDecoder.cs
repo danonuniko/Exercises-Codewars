277247765dbf4e4f444cb5953ea2800270f91729
Daniel Collado <dcolladovizcarro@gmail.com>
2023-05-25 20:03:27 +0200

    Add community solutions RomanNumeralsDecoder.cs

--- Kata 6/RomanNumeralsDecoder/RomanNumeralsDecoder.cs
+++ Kata 6/RomanNumeralsDecoder/RomanNumeralsDecoder.cs
@@ -72,6 +72,55 @@ public class RomanDecode
         return result;
     }
 
+    // Solución comunidad 1. Repasar lógica.
+    private static readonly Dictionary<char, int> literals = new Dictionary<char, int> { {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}, };
+	public static int Solution(string roman)
+	{
+		int result = 0;
+		int max = 0;
+
+		foreach (var c in roman.Reverse())
+		{
+			int value = literals[c];
+
+			if (value < max)        result -= value;
+			else
+			{
+				result += value;
+				max = value;
+			}
+		}
+		return result;
+	}
+
+    // Solución comunidad 2.
+    public static int Solution(string roman)
+    {
+        return roman
+            .Replace("CM", "DCCCC")
+            .Replace("CD", "CCCC")
+            .Replace("XC", "LXXXX")
+            .Replace("XL", "XXXX")
+            .Replace("IX", "VIIII")
+            .Replace("IV", "IIII")
+            .Sum(c => Translate(c));
+    }
+
+    public static int Translate(char c)
+    {
+        switch (c)
+        {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+
     public static void main(String[] args) {
 
         Console.WriteLine(RomanToNumber("XIX"));
